6a8eeeda91b07972dd330f4287c4a104bd2c9100
Steven Diamante <StevenDiamante13@gmail.com>
2021-08-08 23:01:04 -0400

    Pass in restaurants and display them in list

--- src/components/__tests__/RestaurantList.spec.js
+++ src/components/__tests__/RestaurantList.spec.js
@@ -3,10 +3,36 @@ import {RestaurantList} from "./../RestaurantList";
 
 describe("Restaurant List", () => {
   it("loads restaurants on first render", () => {
+    const restaurants = [];
     const loadRestaurants = jest.fn().mockName("loadRestaurants");
 
-    render(<RestaurantList loadRestaurants={loadRestaurants} />);
+    render(
+      <RestaurantList
+        loadRestaurants={loadRestaurants}
+        restaurants={restaurants}
+      />,
+    );
 
     expect(loadRestaurants).toHaveBeenCalled();
+  });
+
+  it("displays restaurants", () => {
+    const noop = () => {};
+    const restaurants = [
+      {
+        id: 1,
+        name: "Sushi Place",
+      },
+      {
+        id: 2,
+        name: "Pizza Place",
+      },
+    ];
+    const {queryByText} = render(
+      <RestaurantList loadRestaurants={noop} restaurants={restaurants} />,
+    );
+
+    expect(queryByText("Sushi Place")).not.toBeNull();
+    expect(queryByText("Pizza Place")).not.toBeNull();
   });
 });
